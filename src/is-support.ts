23279c6da88c71069fac3c0093aac4c7b22c8e5b
STARTRACEX <startraceebr@foxmail.com>
2024-02-29 12:36:08 +0800

    Update isSupport

--- src/is-support.ts
+++ src/is-support.ts
@@ -1,16 +1,16 @@
 const browsers = [
-  { name: "Firefox", regex: /Firefox\/(\d+\.\d+)/, version: 117 },
-  { name: "Edge", regex: /Edg\/(\d+\.\d+)/, version: 120 },
-  { name: "Opera", regex: /OPR\/(\d+\.\d+)/, version: 106 },
-  { name: "Safari", regex: /Version\/(\d+\.\d+).*Safari/, version: 17.2 },
-  { name: "Chrome", regex: /Chrome\/(\d+\.\d+)/, version: 120 },
-];
+  [/Firefox\/(\d+\.\d+)/, 117],
+  [/Edg\/(\d+\.\d+)/, 120],
+  [/OPR\/(\d+\.\d+)/, 106],
+  [/Version\/(\d+\.\d+).*Safari/, 17.2],
+  [/Chrome\/(\d+\.\d+)/, 120],
+] as const;
 
 export function isSupport() {
-  for (const browser of browsers) {
-    const match = navigator.userAgent.match(browser.regex);
+  for (const [regex, version] of browsers) {
+    const match = navigator.userAgent.match(regex);
     if (match) {
-      return Number(match[1]) >= browser.version;
+      return Number(match[1]) >= version;
     }
   }
   return false;
