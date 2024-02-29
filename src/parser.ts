7275d90b688e3ab710e07257981ff5bf4a0e90c5
STARTRACEX <startraceebr@foxmail.com>
2024-02-29 20:11:22 +0800

    Update parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,7 @@
 export interface ParseResult {
   selector: string;
   content: string;
+  deep: number;
   index: number;
   children: ParseResult[];
 }
@@ -10,17 +11,19 @@ export class Parser {
   index: number = 0;
   result: ParseResult;
 
-  _stack: ParseResult[] = [];
-  _selector: string;
-  _selectorStart: number = 0;
-  _contentStart: number = 0;
-  _contentEnd: number = 0;
+  private _stack: ParseResult[] = [];
+  private _selector: string;
+  private _selectorStart: number = 0;
+  private _contentStart: number = 0;
+  private _contentEnd: number = 0;
+  private _deep = 0;
 
   constructor(
     raw: string,
     result: ParseResult = {
       selector: "",
       content: "",
+      deep: 0,
       index: 0,
       children: [],
     }
@@ -58,12 +61,14 @@ export class Parser {
       }
 
       if (char === "{") {
+        this._deep++;
         this._selector = this.raw.slice(this._selectorStart, this.index);
         this._selectorStart = this.index + 1;
         this._contentStart = this.index + 1;
         const childResult: ParseResult = {
           selector: this._selector,
           content: "",
+          deep: this._deep,
           index: this.index,
           children: [],
         };
@@ -73,6 +78,7 @@ export class Parser {
       }
 
       if (char === "}") {
+        this._deep--;
         if (this._stack.length > 0) {
           const parent = this._stack.pop();
           if (parent) {
@@ -90,15 +96,12 @@ export class Parser {
 
   unnest(
     result: ParseResult = this.result,
-    parentSelector: string = "",
-    level = 0
+    parentSelector: string = ""
   ): string {
     let CSSResult = "";
-
     let currentSelector = result.selector.trim();
-
     if (currentSelector === "&") {
-      if (level === 1) {
+      if (result.deep === 1) {
         currentSelector = ":scope";
       } else {
         currentSelector = parentSelector + ":is(" + parentSelector + ")";
@@ -115,7 +118,7 @@ export class Parser {
     }
 
     for (const child of result.children) {
-      const childCSS = this.unnest(child, currentSelector, level + 1);
+      const childCSS = this.unnest(child, currentSelector);
       CSSResult += childCSS;
     }
 
