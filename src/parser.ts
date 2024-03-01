7e5dca32c2babb74623d8fc446bcde7826c47794
STARTRACEX <startraceebr@foxmail.com>
2024-03-01 20:58:14 +0800

    Remove redundant fields

--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,6 @@
+const LeftBrance = "{";
+const RightBrace = "}";
+
 export interface ParseResult {
   selector: string;
   content: string;
@@ -11,11 +14,10 @@ export class Parser {
   index: number = 0;
   result: ParseResult;
 
-  private _stack: ParseResult[] = [];
+  private _lastStack: ParseResult[] = [];
   private _selector: string;
   private _selectorStart: number = 0;
   private _contentStart: number = 0;
-  private _contentEnd: number = 0;
   private _deep = 0;
 
   constructor(
@@ -36,9 +38,10 @@ export class Parser {
     for (this.index = 0; this.index < this.raw.length; this.index++) {
       const char = this.raw[this.index];
 
-      if (char === "'" || char === `"`) {
+      if (char === "'" || char === '"') {
         const qchar = char;
-        for (this.index++; this.index < this.raw.length; this.index++) {
+        this.index++;
+        for (; this.index < this.raw.length; this.index++) {
           const c = this.raw[this.index];
           if (c === "\\") {
             this.index++;
@@ -52,15 +55,14 @@ export class Parser {
 
       if (char === ";") {
         this._selectorStart = this.index + 1;
-        this._contentEnd = this.index;
         this.result.content += this.raw.slice(
           this._contentStart,
-          this._contentEnd + 1
+          this.index + 1
         );
         continue;
       }
 
-      if (char === "{") {
+      if (char === LeftBrance) {
         this._deep++;
         this._selector = this.raw.slice(this._selectorStart, this.index);
         this._selectorStart = this.index + 1;
@@ -72,15 +74,15 @@ export class Parser {
           index: this.index,
           children: [],
         };
-        this._stack.push(this.result);
+        this._lastStack.push(this.result);
         this.result = childResult;
         continue;
       }
 
-      if (char === "}") {
+      if (char === RightBrace) {
         this._deep--;
-        if (this._stack.length > 0) {
-          const parent = this._stack.pop();
+        if (this._lastStack.length > 0) {
+          const parent = this._lastStack.pop();
           if (parent) {
             parent.children.push(this.result);
             this.result = parent;
@@ -91,6 +93,7 @@ export class Parser {
         continue;
       }
     }
+
     return this.result;
   }
 
@@ -98,7 +101,7 @@ export class Parser {
     result: ParseResult = this.result,
     parentSelector: string = ""
   ): string {
-    let CSSResult = "";
+    let unnestResult = "";
     let currentSelector = result.selector.trim();
     if (currentSelector === "&") {
       if (result.deep === 1) {
@@ -114,15 +117,16 @@ export class Parser {
 
     let currentContent = result.content.trim();
     if (currentContent) {
-      CSSResult += currentSelector + "{" + currentContent + "}";
+      unnestResult +=
+        currentSelector + LeftBrance + currentContent + RightBrace;
     }
 
     for (const child of result.children) {
       const childCSS = this.unnest(child, currentSelector);
-      CSSResult += childCSS;
+      unnestResult += childCSS;
     }
 
-    return CSSResult;
+    return unnestResult;
   }
 }
 
