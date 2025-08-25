5c28dc47d4fe133aa0912eb548a5cbd334a5df5b
Shiro Wang <startraceebr@foxmail.com>
2025-08-26 04:12:21 +0800

    feat: replace character codes with strings in the token state

--- src/lib/host.ts
+++ src/lib/host.ts
@@ -1,9 +1,20 @@
-import { type Element, RULESET, Tokenizer } from "./stylis.ts";
+import {
+  COLON,
+  type Element,
+  GREATER_THAN,
+  LEFT_SQUARE_BRACKET,
+  PLUS,
+  RIGHT_SQUARE_BRACKET,
+  RULESET,
+  SPACE,
+  TILDE,
+  Tokenizer,
+} from "./stylis.ts";
 
 const _host = "host";
 
 export const host = (element: Element): void => {
-  if (element.type === RULESET && element.parent?.value.startsWith(":" + _host)) {
+  if (element.type === RULESET && element.parent?.value.startsWith(COLON + _host)) {
     element.props = (element.props as string[]).map((item) => {
       const tokens = new Tokenizer(item).tokenize();
       let start: number | undefined,
@@ -12,22 +23,23 @@ export const host = (element: Element): void => {
       for (let index = 0; index < tokens.length; index++) {
         const token = tokens[index];
         switch (token) {
-          case ":":
+          case COLON:
             if (tokens.length > index + 2 && tokens[index + 1] === _host) {
               const afterHostToken = tokens[index + 2];
               if (
-                afterHostToken === ":" ||
-                (afterHostToken.startsWith("[") && afterHostToken.endsWith("]"))
+                afterHostToken === COLON ||
+                (afterHostToken.startsWith(LEFT_SQUARE_BRACKET) &&
+                  afterHostToken.endsWith(RIGHT_SQUARE_BRACKET))
               ) {
                 start = index + 2;
                 hasHost = true;
               }
             }
             break;
-          case " ":
-          case ">":
-          case "~":
-          case "+":
+          case SPACE:
+          case GREATER_THAN:
+          case TILDE:
+          case PLUS:
             if (hasHost) {
               end = index;
             }
